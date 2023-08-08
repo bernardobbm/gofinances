2a0263c90264157a5ee8abc001b6eb44c519b9ac
Bernardo <bernardobbm334@gmail.com>
2023-08-08 18:47:22 -0300

    :sparkles: feat: Adição de componentes de header para a página inicial

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,35 @@
-import { ViewProps } from 'react-native';
-import { Container, Title } from './styles';
+import { ViewProps } from 'react-native'
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+} from './styles'
 
-interface DashboardProps extends ViewProps {}
+type DashboardProps = ViewProps
 
 export function Dashboard({ ...rest }: DashboardProps) {
-	return (
-		<Container {...rest}>
-			<Title>Dashboard</Title>
-		</Container>
-	);
+  return (
+    <Container {...rest}>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/114936465?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Bernamassa</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
+    </Container>
+  )
 }
