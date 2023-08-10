1c569b1e8d2b4dc98194895d798f32695ec8a506
Bernardo <bernardobbm334@gmail.com>
2023-08-09 21:03:13 -0300

    :ok_hand: style: Adicionada restrição de lint

--- App.tsx
+++ App.tsx
@@ -1,39 +1,40 @@
-import * as SplashScreen from 'expo-splash-screen';
-import { useCallback } from 'react';
-import { ThemeProvider } from 'styled-components/native';
+/* eslint-disable camelcase */
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback } from 'react'
+import { ThemeProvider } from 'styled-components/native'
 
 import {
-	useFonts,
-	Poppins_400Regular,
-	Poppins_500Medium,
-	Poppins_700Bold,
-} from '@expo-google-fonts/poppins';
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins'
 
-import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
+import theme from './src/global/styles/theme'
+import { Dashboard } from './src/screens/Dashboard'
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-	const [fontsLoaded] = useFonts({
-		Poppins_400Regular,
-		Poppins_500Medium,
-		Poppins_700Bold,
-	});
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  })
 
-	const onLayoutRootView = useCallback(async () => {
-		if (fontsLoaded) {
-			await SplashScreen.hideAsync();
-		}
-	}, [fontsLoaded]);
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
 
-	if (!fontsLoaded) {
-		return null;
-	}
+  if (!fontsLoaded) {
+    return null
+  }
 
-	return (
-		<ThemeProvider theme={theme}>
-			<Dashboard onLayout={onLayoutRootView} />
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard onLayout={onLayoutRootView} />
+    </ThemeProvider>
+  )
 }
