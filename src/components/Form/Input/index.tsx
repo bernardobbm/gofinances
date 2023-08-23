4e3d5c168ba2f8c9cbbce18cb64cc325535bcc47
Bernardo <bernardobbm334@gmail.com>
2023-08-22 22:59:56 -0300

    :sparkles: feat: Componente de formulário e input criados

---
+++ src/components/Form/Input/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from './styles'
+import { TextInputProps } from 'react-native'
+
+type InputProps = TextInputProps
+
+export function Input({ ...rest }: InputProps) {
+  return <Container {...rest}></Container>
+}
