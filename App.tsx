0cc46c3aa6b1fd2144ce3fec722446a60629688e
Bernardo <bernardobbm334@gmail.com>
2023-08-22 23:01:22 -0300

    :sparkles: feat: App.tsx editado momentaneamente para construção da página de Cadastro

--- App.tsx
+++ App.tsx
@@ -11,7 +11,8 @@ import {
 } from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme'
-import { Dashboard } from './src/screens/Dashboard'
+import { Register } from './src/screens/Register'
+// import { Dashboard } from './src/screens/Dashboard'
 
 SplashScreen.preventAutoHideAsync()
 
@@ -34,7 +35,8 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard onLayout={onLayoutRootView} />
+      {/* <Dashboard onLayout={onLayoutRootView} /> */}
+      <Register onLayout={onLayoutRootView} />
     </ThemeProvider>
   )
 }
